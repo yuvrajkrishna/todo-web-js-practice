5c2690e67dc71ce5506421b85fdafd5bdf824a5b
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-06-26 21:12:19 +0530

    added the feature of adding task

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-//  Adding validation for empty field 
-document.querySelector('#push').onclick = function 
-(){
-    if(document.querySelector('#newtask input').value.length === 0) {
-        alert("Please Enter a Task")
+let input = document.querySelector("#newtask input")
+let push = document.querySelector("#push")
+let alltasks = document.querySelector("#tasks")
+
+function addTask(){
+    if(input.value.trim() === ""){
+        alert("Please Enter Task to do....");
     }
     else{
-        document.querySelector('#tasks').innerHTML += `
-            <div class="task">
-                <span id="taskname">
-                    ${document.querySelector('#newtask input').value}
-                </span>
-            </div>
-        `;
+        alltasks.innerHTML += `
+        <div class = "task">
+            <span class="taskname">
+                ${input.value}
+            </span>
+            <button class="delete">
+                <i class="ri-delete-bin-6-line"></i>
+            </button>
+        </div>
+        `
+        input.value = ""
     }
 }
+
+push.addEventListener("click",addTask)
+
